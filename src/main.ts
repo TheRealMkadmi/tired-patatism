940167f4eb2ff35b4de64ee0e8a7a16c4bcad0a3
chihabhajji <blackpang23@gmail.com>
2023-08-10 00:47:33 +0100

    :sparkles: Refactor code and fix linting issues
    Refactor the code in `src/main.ts` to improve readability and fix linting issues. Also, fix the import statement for `ConsoleLogger` in the same file.
    
    :bug: Fix typo in nest-cli.json
    
    Fix a typo in the `nest-cli.json` file by replacing the file extension from `.schema.ts` to `.entity.ts` in the `dtoFileNameSuffix` options.
    
    :recycle: Clean up form-builder.controller.ts
    
    Clean up the code in `src/modules/form-builder/form-builder.controller.ts` by removing unnecessary whitespace in the constructor.

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,37 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {patchNestJsSwagger} from "nestjs-zod";
+import { ConsoleLogger } from '@nestjs/common';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config()
-
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-
-    patchNestJsSwagger();
+  const logger = new ConsoleLogger();
+  logger.setContext('Bootstrap');
+  app.enableCors({
+    origin: '*',
+  });
+  patchNestJsSwagger();
   const config = new DocumentBuilder()
     .setTitle('NestJS API')
     .setVersion('1.0')
     .build();
-    SwaggerModule.setup(
-        'api',
-        app,
-        SwaggerModule.createDocument(app, config, {
-            operationIdFactory: (controllerKey, methodKey) => methodKey,
-            deepScanRoutes: true,
-        }),
-        {
-            yamlDocumentUrl: '/yaml',
-        },
-    );
+  SwaggerModule.setup(
+    'api',
+    app,
+    SwaggerModule.createDocument(app, config, {
+      operationIdFactory: (controllerKey, methodKey) => methodKey,
+      deepScanRoutes: true,
+    }),
+    {
+      yamlDocumentUrl: '/yaml',
+    },
+  );
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(PORT || 3000, () =>
+    logger.log(`Server started on port http://localhost:${PORT}`),
+  );
+  logger.log(`Swagger UI is available at http://localhost:${PORT}/api`);
 }
 bootstrap();
