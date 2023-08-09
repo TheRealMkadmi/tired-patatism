a1137d01bd7ab13bbb20ac7052f05a15818fd628
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2023-08-10 00:47:30 +0100

    🔧 chore(main.ts): enable CORS with wildcard origin in the bootstrap function Enabling CORS (Cross-Origin Resource Sharing) with a wildcard origin allows requests from any domain. This change is necessary to allow the application to handle requests from different origins, which is particularly useful during development when the frontend and backend are hosted on different domains.

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ require('dotenv').config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
 
 
     patchNestJsSwagger();
