65071d77f79e3326cc2910901cb0e1e0d95f22cc
chihabhajji <blackpang23@gmail.com>
2023-08-10 11:42:31 +0100

    🚀 :sparkles: :lipstick: :art: :pencil2: :white_check_mark: :construction: :wrench: :bug: :fire: :arrow_up: :arrow_down:
    feat(form-submissions): Add _form property to CreateFormSubmissionDto
    
    feat(form-submissions): Add SubmissionField entity
    
    fix(form-builder): Fix typo in parameter name
    
    feat(form-submissions): Add submissionFields property to FormSubmission entity

--- src/modules/form-builder/entities/component.entity.ts
+++ src/modules/form-builder/entities/component.entity.ts
@@ -1,5 +1,6 @@
 import {Prop} from "@nestjs/mongoose";
 import {ComponentType} from "@/form-builder/constants/form-builder-enums.";
+import {Types} from "mongoose";
 
 export class Component {
     @Prop({ required: true, type: String })
@@ -8,4 +9,6 @@ export class Component {
     key: string;
     @Prop({ required: true, type: String, enum: ComponentType })
     type: ComponentType;
+    @Prop({ type: Types.Array})
+    options?: string[];
 }
