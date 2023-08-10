70d74c8fc392fef207aae7f9276caf67c8682106
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2023-08-10 01:54:06 +0100

    ✨ feat(form-builder.module.ts): add FormSubmission entity to MongooseModule imports The FormSubmission entity and its schema are now imported into the form-builder.module.ts file. This allows the application to interact with the FormSubmission data in the database, enabling the creation, reading, updating, and deletion of form submissions. This change is necessary to support the new feature of handling form submissions.

--- src/modules/form-builder/form-builder.module.ts
+++ src/modules/form-builder/form-builder.module.ts
@@ -3,10 +3,14 @@ import { FormBuilderService } from './form-builder.service';
 import { FormBuilderController } from './form-builder.controller';
 import { MongooseModule } from "@nestjs/mongoose";
 import { FormBuilder, FormBuilderSchema } from "@/form-builder/entities/form-builder.entity";
+import { FormSubmission, FormSubmissionSchema } from "@/form-submissions/entities/form-submission.entity";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: FormBuilder.name, schema: FormBuilderSchema }]),
+    MongooseModule.forFeature([
+      { name: FormBuilder.name, schema: FormBuilderSchema },
+      { name: FormSubmission.name, schema: FormSubmissionSchema }
+    ]),
   ],
   controllers: [FormBuilderController],
   providers: [FormBuilderService],
