3fae827d54b8321648e2257d26ef651cfe8a87a2
chihabhajji <blackpang23@gmail.com>
2023-08-10 01:27:54 +0100

    🎨 src: add event emitter module
    The commit adds the event emitter module to the app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
-import { ConsoleLogger, Module } from "@nestjs/common";
-import { UserModule } from '@user/user.module';
-import { classes } from '@automapper/classes';
-import { MongooseModule } from "@nestjs/mongoose";
-import { AutomapperModule } from "@timonmasberg/automapper-nestjs";
-import { FormSubmissionsModule } from "@/form-submissions/form-submissions.module";
-import { FormBuilderModule } from "@/form-builder/form-builder.module";
-import { APP_FILTER } from "@nestjs/core";
-import { ExceptionLoggerService } from "@common/services/exception-logger";
-import { GlobalExceptionFilter } from "@common/filters/global-exception";
+import {Module} from "@nestjs/common";
+import {UserModule} from '@user/user.module';
+import {classes} from '@automapper/classes';
+import {MongooseModule} from "@nestjs/mongoose";
+import {AutomapperModule} from "@timonmasberg/automapper-nestjs";
+import {FormSubmissionsModule} from "@/form-submissions/form-submissions.module";
+import {FormBuilderModule} from "@/form-builder/form-builder.module";
+import {APP_FILTER} from "@nestjs/core";
+import {ExceptionLoggerService} from "@common/services/exception-logger";
+import {GlobalExceptionFilter} from "@common/filters/global-exception";
+import {EventEmitterModule} from '@nestjs/event-emitter';
+import {config} from "dotenv";
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config()
+config()
 
 @Module({
   imports: [
@@ -18,14 +19,15 @@ require('dotenv').config()
     AutomapperModule.forRoot({
       strategyInitializer: classes(),
     }),
-
+    EventEmitterModule.forRoot({
+      global: true,
+    }),
     UserModule,
     FormSubmissionsModule,
-    FormBuilderModule
+    FormBuilderModule,
   ],
   controllers: [],
   providers: [
-    ConsoleLogger,
     ExceptionLoggerService,
     {
       provide: APP_FILTER,
